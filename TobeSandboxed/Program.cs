dd57b9d24724a18fcb7704d35f326791194c6218
akano <akano@DESKTOP-SEOOBGI>
2023-03-06 07:52:35 -0800

    Add project files.

---
+++ TobeSandboxed/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TobeSandboxed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I ran in a sandbox");
+        }
+
+        public class UntrustedClass
+        {
+            // Pretend to be a method checking if a number is a Fibonacci
+            // but which actually attempts to read a file.
+            public static bool IsFibonacci(int number)
+            {
+                File.ReadAllText("C:\\Temp\\file.txt");
+                
+                return false;
+            }
+
+            public static string PrintMyNameAsync(string firstname, string surname)
+            {
+                File.ReadAllText("C:\\Temp\\file.txt");
+                string lines =
+                
+                    "First line, Second line, Third line"
+                ;
+                File.WriteAllText("WriteLines.txt", lines);
+
+                return "My name is " + firstname + " " + surname;
+            }
+
+            public static async Task<string> makewebrequest()
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+                //client.DefaultRequestHeaders.Accept.Add(
+                //    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+                //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+                var json = await client.GetStringAsync(
+                                        "https://api.github.com/orgs/dotnet/repos"
+                                        );
+
+                return "just json";
+            }
+        }
+
+        public class AnotherUntrustedClass
+        {
+            public void makesomenoise(string sound)
+            {
+                Console.WriteLine(sound);
+            }
+        }
+    }
+}
